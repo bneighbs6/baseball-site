c88f33c02990a235289c6b9234e12236cb4b176e
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-03-24 21:23:42 -0600

    Created ScheduleForm

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import Header from './Components/Header';
 import NavigationBar from './Components/NavigationBar';
+import ScheduleForm from './Components/ScheduleForm';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <NavigationBar />
+      <div id="schedule">
+        <ScheduleForm />
+      </div>
     </div>
   );
 }
---
+++ src/Components/ScheduleForm.jsx
@@ -0,0 +1,142 @@
+import React, { useEffect, useState } from "react";
+import { Button, Dropdown, Form } from "react-bootstrap";
+
+function ScheduleForm() {
+
+  const initialFormState = {
+    parent_name: "",
+    athlete_name: "",
+    parent_email: "",
+    lesson_duration: "",
+    request: "",
+  };
+
+  const [form, setForm] = useState({ ...initialFormState });
+  const [lessonDuration, setLessonDuration] = useState("");
+
+  async function handleSubmit(e) {
+    e.preventDefault(); // Prevent the default form submission
+    console.log("Submit clicked.");
+
+    // Send form data to Formspree
+    const response = await fetch("https://formspree.io/f/xovelaak", {
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(form),
+    });
+
+    if (response.ok) {
+      alert(
+        "Your form has been submitted. You will receive an email responding to your request within 24 hours."
+      );
+      setForm({ ...initialFormState }); // Reset the form
+    } else {
+      alert("There was a problem with your submission. Please try again.");
+    }
+  }
+
+  function handleParentNameChange(e) {
+    setForm({ ...form, parent_name: e.target.value });
+  }
+
+  function handleAthleteNameChange(e) {
+    setForm({ ...form, athlete_name: e.target.value });
+  }
+
+  function handleParentEmailChange(e) {
+    setForm({ ...form, parent_email: e.target.value });
+  }
+
+  function handleLessonDurationChange(e) {
+    setForm({ ...form, lesson_duration: e.target.value });
+  }
+
+  function handleRequestChange(e) {
+    setForm({ ...form, request: e.target.value });
+  }
+
+  return (
+    <>
+      <h1>Schedule a Lesson</h1>
+      <h4>Please fill out the form below. You will receive a response from Coach Brett within 24 hours.</h4>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group className="mb-3">
+          <Form.Label>Parent Name</Form.Label>
+          <Form.Control
+            name="parent_name" // Changed to match the state key
+            type="text"
+            required
+            value={form.parent_name}
+            onChange={handleParentNameChange}
+            placeholder="Enter Parent's Name"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Athlete's Name</Form.Label>
+          <Form.Control
+            name="athlete_name" // Changed to match the state key
+            type="text"
+            required
+            value={form.athlete_name}
+            onChange={handleAthleteNameChange}
+            placeholder="Enter your street address"
+          />
+        </Form.Group>
+        <Form.Group className="mb-3">
+          <Form.Label>Parent's Email Address</Form.Label>
+          <Form.Control
+            name="parent_email" // Changed to match the state key
+            type="email"
+            required
+            value={form.parent_email}
+            onChange={handleParentEmailChange}
+            placeholder="Enter Parent's Email"
+          />
+        </Form.Group>
+
+
+        <Form.Group className="mb-3">
+          <Dropdown onSelect={handleLessonDurationChange}>
+            <Dropdown.Toggle 
+            variant="primary"
+            style={{ backgroundColor: "#003f85", border: "none", color: "white" }}
+            >{lessonDuration || "Choose how long you want to train"}</Dropdown.Toggle>
+            <Dropdown.Menu>
+                <Dropdown.Item disabled>*Choose how long you want to train</Dropdown.Item>
+                <Dropdown.Item eventKey={"30 Min"}>30 Min</Dropdown.Item>
+                <Dropdown.Item eventKey={"60 Min"}>60 Min</Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </Form.Group>
+
+
+        <Form.Group className="mb-3">
+          <Form.Label>Tell us what you are looking for your athlete.</Form.Label>
+          <Form.Control
+            name="request"
+            as="textarea"
+            required
+            rows={2}
+            value={form.request}
+            onChange={handleRequestChange}
+            placeholder="e.g. 'I am looking for hitting lessons twice a week for four weeks for my son.'"
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group>
+          <Button
+            variant="primary"
+            style={{ backgroundColor: "#003f85", border: "none", color: "white" }}
+            type="submit"
+          >
+            Submit
+          </Button>
+        </Form.Group>
+      </Form>
+    </>
+  );
+}
+
+export default ScheduleForm;
