065d2221b1c699d11ae079fc04c1154d95ba2f02
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-03-25 11:00:29 -0600

    Create MeetTheCoach; Place each component in a div

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import './App.css';
 import Header from './Components/Header';
+import MeetTheCoach from './Components/MeetTheCoach';
 import NavigationBar from './Components/NavigationBar';
 import ScheduleForm from './Components/ScheduleForm';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <NavigationBar />
+      <div className="header my-3">
+        <Header />
+      </div>
+      <div className="navbar mx-auto">
+        <NavigationBar />
+      </div>
+      <div className="meetTheCoach">
+        <MeetTheCoach />
+      </div>
       <div id="schedule">
         <ScheduleForm />
       </div>
---
+++ src/Components/MeetTheCoach.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function MeetTheCoach() {
+    return (
+        <div>
+            <h1>Meet the Coach</h1>
+            <p>Paragraph about my "stellar" baseball career.</p>
+        </div>
+    );
+}
+
+export default MeetTheCoach;
