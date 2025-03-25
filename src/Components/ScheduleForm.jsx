b6b1dac7d2d1e731835b3fae4b14e81f604aebb8
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-03-24 21:37:17 -0600

    Editted styling of schedule form

--- src/Components/ScheduleForm.jsx
+++ src/Components/ScheduleForm.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Button, Dropdown, Form } from "react-bootstrap";
+import { Button, Dropdown, Form, Card } from "react-bootstrap";
 
 function ScheduleForm() {
 
@@ -50,8 +50,9 @@ function ScheduleForm() {
     setForm({ ...form, parent_email: e.target.value });
   }
 
-  function handleLessonDurationChange(e) {
-    setForm({ ...form, lesson_duration: e.target.value });
+  function handleLessonDurationChange(eventKey) {
+    setLessonDuration(eventKey); // Update the lesson duration state
+    setForm({ ...form, lesson_duration: eventKey }); // Update the form state
   }
 
   function handleRequestChange(e) {
@@ -59,83 +60,101 @@ function ScheduleForm() {
   }
 
   return (
-    <>
-      <h1>Schedule a Lesson</h1>
-      <h4>Please fill out the form below. You will receive a response from Coach Brett within 24 hours.</h4>
-      <Form onSubmit={handleSubmit}>
-        <Form.Group className="mb-3">
-          <Form.Label>Parent Name</Form.Label>
-          <Form.Control
-            name="parent_name" // Changed to match the state key
-            type="text"
-            required
-            value={form.parent_name}
-            onChange={handleParentNameChange}
-            placeholder="Enter Parent's Name"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Athlete's Name</Form.Label>
-          <Form.Control
-            name="athlete_name" // Changed to match the state key
-            type="text"
-            required
-            value={form.athlete_name}
-            onChange={handleAthleteNameChange}
-            placeholder="Enter your street address"
-          />
-        </Form.Group>
-        <Form.Group className="mb-3">
-          <Form.Label>Parent's Email Address</Form.Label>
-          <Form.Control
-            name="parent_email" // Changed to match the state key
-            type="email"
-            required
-            value={form.parent_email}
-            onChange={handleParentEmailChange}
-            placeholder="Enter Parent's Email"
-          />
-        </Form.Group>
+    <div className="schedule-form mx-5">
+      <Card>
+        <h1>Schedule a Lesson</h1>
+        <h4>
+          Please fill out the form below. You will receive a response from a
+          coach within 24 hours.
+        </h4>
+        <Form onSubmit={handleSubmit}>
+          <Form.Group className="mb-3">
+            <Form.Label>Parent Name</Form.Label>
+            <Form.Control
+              name="parent_name" // Changed to match the state key
+              type="text"
+              required
+              value={form.parent_name}
+              onChange={handleParentNameChange}
+              placeholder="Enter Parent's Name"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Athlete's Name</Form.Label>
+            <Form.Control
+              name="athlete_name" // Changed to match the state key
+              type="text"
+              required
+              value={form.athlete_name}
+              onChange={handleAthleteNameChange}
+              placeholder="Enter your street address"
+            />
+          </Form.Group>
+          <Form.Group className="mb-3">
+            <Form.Label>Parent's Email Address</Form.Label>
+            <Form.Control
+              name="parent_email" // Changed to match the state key
+              type="email"
+              required
+              value={form.parent_email}
+              onChange={handleParentEmailChange}
+              placeholder="Enter Parent's Email"
+            />
+          </Form.Group>
 
-
-        <Form.Group className="mb-3">
-          <Dropdown onSelect={handleLessonDurationChange}>
-            <Dropdown.Toggle 
-            variant="primary"
-            style={{ backgroundColor: "#003f85", border: "none", color: "white" }}
-            >{lessonDuration || "Choose how long you want to train"}</Dropdown.Toggle>
-            <Dropdown.Menu>
-                <Dropdown.Item disabled>*Choose how long you want to train</Dropdown.Item>
+          <Form.Group className="mb-3">
+            <Form.Label>Lesson Duration</Form.Label>
+            <Dropdown onSelect={handleLessonDurationChange}>
+              <Dropdown.Toggle
+                variant="primary"
+                style={{
+                  backgroundColor: "#003f85",
+                  border: "none",
+                  color: "white",
+                }}
+              >
+                {lessonDuration || "Choose how long you want to train"}
+              </Dropdown.Toggle>
+              <Dropdown.Menu>
+                <Dropdown.Item>
+                  *Choose how long you want to train
+                </Dropdown.Item>
                 <Dropdown.Item eventKey={"30 Min"}>30 Min</Dropdown.Item>
                 <Dropdown.Item eventKey={"60 Min"}>60 Min</Dropdown.Item>
-            </Dropdown.Menu>
-          </Dropdown>
-        </Form.Group>
+              </Dropdown.Menu>
+            </Dropdown>
+          </Form.Group>
 
-
-        <Form.Group className="mb-3">
-          <Form.Label>Tell us what you are looking for your athlete.</Form.Label>
-          <Form.Control
-            name="request"
-            as="textarea"
-            required
-            rows={2}
-            value={form.request}
-            onChange={handleRequestChange}
-            placeholder="e.g. 'I am looking for hitting lessons twice a week for four weeks for my son.'"
-          ></Form.Control>
-        </Form.Group>
-        <Form.Group>
-          <Button
-            variant="primary"
-            style={{ backgroundColor: "#003f85", border: "none", color: "white" }}
-            type="submit"
-          >
-            Submit
-          </Button>
-        </Form.Group>
-      </Form>
-    </>
+          <Form.Group className="mb-3">
+            <Form.Label>
+              Tell us what you are looking for your athlete.
+            </Form.Label>
+            <Form.Control
+              name="request"
+              as="textarea"
+              required
+              rows={2}
+              value={form.request}
+              onChange={handleRequestChange}
+              placeholder="e.g. 'I am looking for hitting lessons twice a week for four weeks for my son.'"
+            ></Form.Control>
+          </Form.Group>
+          <Form.Group>
+            <Button
+              variant="primary"
+              style={{
+                backgroundColor: "#003f85",
+                border: "none",
+                color: "white",
+              }}
+              type="submit"
+            >
+              Submit
+            </Button>
+          </Form.Group>
+        </Form>
+      </Card>
+    </div>
   );
 }
 
