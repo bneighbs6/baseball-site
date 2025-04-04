453e4f35c1cfe76679988042430967d9c1f8fa1b
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-03 21:34:21 -0600

    Styled CalendlyForm.jsx

--- src/Components/CalendlyForm.jsx
+++ src/Components/CalendlyForm.jsx
@@ -6,19 +6,42 @@ function Calendly() {
       <div className="calendly mx-5">
         <Card xs="9">
           <Card.Body>
-            <Card.Title><h3>Schedule a Lesson</h3></Card.Title>
+            <Card.Title>
+              <div className="lesson-header">
+                <h3>Schedule a Lesson</h3>
+              </div>
+            </Card.Title>
             <Card.Text>
-              Click on the duration you want your lesson to be and you will be redirected to my calendar to set up a time for your lesson.
+              Click on the duration you want your lesson to be and you will be
+              redirected to my calendar to set up a time for your lesson.
             </Card.Text>
+            <Card.Body>
+              <button className="lesson-btn">30 Minute Lesson</button>
+              <button className="lesson-btn">60 Minute Lesson</button>
+            </Card.Body>
           </Card.Body>
+          <h3 className="pricing-header">Pricing</h3>
           <ListGroup className="list-group-flush">
-          <ListGroup.Item><h4>Prices</h4></ListGroup.Item>
+            <ListGroup.Item>
+              <h4>One Athlete</h4>
+            </ListGroup.Item>
             <ListGroup.Item>30 Min Lesson = $60</ListGroup.Item>
             <ListGroup.Item>60 Min Lesson = $100</ListGroup.Item>
           </ListGroup>
-          <Card.Body>
-                <button className="lesson-btn">30 Minute Lesson</button><button className="lesson-btn">60 Minute Lesson</button>
-          </Card.Body>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>
+              <h4>Two Athletes</h4>
+            </ListGroup.Item>
+            <ListGroup.Item>30 Min Lesson = $100</ListGroup.Item>
+            <ListGroup.Item>60 Min Lesson = $160</ListGroup.Item>
+          </ListGroup>
+          <ListGroup className="list-group-flush">
+            <ListGroup.Item>
+              <h4>Three Athletes</h4>
+            </ListGroup.Item>
+            <ListGroup.Item>30 Min Lesson = $120</ListGroup.Item>
+            <ListGroup.Item>60 Min Lesson = $180</ListGroup.Item>
+          </ListGroup>
         </Card>
       </div>
     );
