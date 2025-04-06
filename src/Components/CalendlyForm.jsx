f6ed66979bf077c1fc913d631f244b9b0d677927
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-05 20:23:05 -0600

    Edit Calendly pricing & btns display

--- src/Components/CalendlyForm.jsx
+++ src/Components/CalendlyForm.jsx
@@ -24,36 +24,40 @@ function Calendly() {
               Click on the duration you want your lesson to be and you will be
               redirected to my calendar to set up a time for your lesson.
             </Card.Text>
-            <Card.Body>
-              <button onClick={handle30MinClick} className="lesson-btn">30 Minute Lesson</button>
-              <button onClick={handle60MinClick} className="lesson-btn">60 Minute Lesson</button>
-            </Card.Body>
+            <div className="flex-container">
+              <div className="pricing-section">
+                <h3 className="pricing-header">Pricing</h3>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>
+                    <h4>One Athlete</h4>
+                  </ListGroup.Item>
+                  <ListGroup.Item>30 Min Lesson = $60</ListGroup.Item>
+                  <ListGroup.Item>60 Min Lesson = $100</ListGroup.Item>
+                </ListGroup>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>
+                    <h4>Two Athletes</h4>
+                  </ListGroup.Item>
+                  <ListGroup.Item>30 Min Lesson = $100</ListGroup.Item>
+                  <ListGroup.Item>60 Min Lesson = $160</ListGroup.Item>
+                </ListGroup>
+                <ListGroup className="list-group-flush">
+                  <ListGroup.Item>
+                    <h4>Three Athletes</h4>
+                  </ListGroup.Item>
+                  <ListGroup.Item>30 Min Lesson = $120</ListGroup.Item>
+                  <ListGroup.Item>60 Min Lesson = $180</ListGroup.Item>
+                </ListGroup>
+              </div>
+              <div className="lesson-buttons">
+                <button onClick={handle30MinClick} className="lesson-btn">30 Minute Lesson</button>
+                <button onClick={handle60MinClick} className="lesson-btn">60 Minute Lesson</button>
+              </div>
+            </div>
           </Card.Body>
-          <h3 className="pricing-header">Pricing</h3>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>
-              <h4>One Athlete</h4>
-            </ListGroup.Item>
-            <ListGroup.Item>30 Min Lesson = $60</ListGroup.Item>
-            <ListGroup.Item>60 Min Lesson = $100</ListGroup.Item>
-          </ListGroup>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>
-              <h4>Two Athletes</h4>
-            </ListGroup.Item>
-            <ListGroup.Item>30 Min Lesson = $100</ListGroup.Item>
-            <ListGroup.Item>60 Min Lesson = $160</ListGroup.Item>
-          </ListGroup>
-          <ListGroup className="list-group-flush">
-            <ListGroup.Item>
-              <h4>Three Athletes</h4>
-            </ListGroup.Item>
-            <ListGroup.Item>30 Min Lesson = $120</ListGroup.Item>
-            <ListGroup.Item>60 Min Lesson = $180</ListGroup.Item>
-          </ListGroup>
         </Card>
       </div>
     );
 }
 
-export default Calendly; 
+export default Calendly;
