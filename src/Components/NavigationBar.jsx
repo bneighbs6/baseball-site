33e75389725d9bc444bb0cf68881ceb6d42d5787
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-10 16:05:14 -0600

    Make each nav.link a <button>

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -7,78 +7,37 @@ function NavigationBar() {
     <Navbar expand="md" variant="light">
       <Container>
         <Nav className="mx-auto">
-          {/* Need to style each nav link directly */}
           <Nav.Link
             as={Link}
             to="header" // Brings user to the top of the webpage if clicked
             smooth={true}
             duration={500}
-            className="nav-link"
-            style={{
-              color: "#00e6e6",
-              textAlign: "center",
-              backgroundColor: "black",
-              padding: "10px 20px",
-              margin: "5px",
-              borderRadius: "5px",
-              textDecoration: "none",
-            }}
           >
-            Home
+            <button className="nav-btn">Home</button>
           </Nav.Link>
-          {/* Need to style each nav link directly */}
           <Nav.Link
             as={Link}
             to="meetTheCoach"
             smooth={true}
             duration={500}
-            style={{
-              color: "#00e6e6",
-              textAlign: "center",
-              backgroundColor: "black",
-              padding: "10px 20px",
-              margin: "5px",
-              borderRadius: "5px",
-              textDecoration: "none",
-            }}
           >
-            Meet The Coach
+            <button className="nav-btn">Meet The Coach</button>
           </Nav.Link>
-          {/* Need to style each nav link directly */}
           <Nav.Link
             as={Link}
-            to="calendly"
+            to="meetTheCoach"
             smooth={true}
             duration={500}
-            style={{
-              color: "#00e6e6",
-              textAlign: "center",
-              backgroundColor: "black",
-              padding: "10px 20px",
-              margin: "5px",
-              borderRadius: "5px",
-              textDecoration: "none",
-            }}
           >
-            Schedule a Lesson
+            <button className="nav-btn">Schedule a Lesson</button>
           </Nav.Link>
-          {/* Need to style each nav link directly */}
           <Nav.Link
             as={Link}
-            to="contact"
+            to="meetTheCoach"
             smooth={true}
             duration={500}
-            style={{
-              color: "#00e6e6",
-              textAlign: "center",
-              backgroundColor: "black",
-              padding: "10px 20px",
-              margin: "5px",
-              borderRadius: "5px",
-              textDecoration: "none",
-            }}
           >
-            Contact Us
+            <button className="nav-btn">Contact Us</button>
           </Nav.Link>
         </Nav>
       </Container>
