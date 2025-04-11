041461519d0b82c780506f509a7eb486a658a01e
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-11 10:26:03 -0600

    Add css to header to create a carousel backgground

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,6 @@
 import React from "react";
-import logoImage from "../media/Transparent.png"
-import fielding1 from "../media/fielding1.jpg"
+import logoImage from "../media/Transparent.png";
+import fielding1 from "../media/fielding1.jpg";
 import fielding2 from "../media/fielding2.jpg";
 import fielding3 from "../media/fieliding3.jpg";
 import leftySwing1 from "../media/leftySwing1.jpg";
@@ -13,39 +13,39 @@ import { Carousel } from "react-bootstrap";
 
 function Header() { 
     return (
-      <header className="header-logo-container text-center">
-        <img className="logo" src={logoImage} alt="Neighborly Nine Logo" />
-        <Carousel>
-          <Carousel.Item className="carousel-item" interval={1000}>
+      <header className="header-container">
+        <Carousel className="carousel-background">
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={fielding1} alt="coach instructing baseball drill" />
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={1000}>
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={fielding2} alt="coach instructing baseball drill" />
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={1000}>
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={fielding3} alt="coach instructing baseball drill" />
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={1000}>
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={leftySwing1} alt="coach instructing baseball drill" />
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={1000}>
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={rightySwing1} alt="coach instructing baseball drill" />
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={1000}>
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={tee1} alt="coach instructing baseball drill" />
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={1000}>
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={tee2} alt="coach instructing baseball drill" />
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={1000}>
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={standingWithBat} alt="coach instructing baseball drill" />
           </Carousel.Item>
-          <Carousel.Item className="carousel-item" interval={1000}>
+          <Carousel.Item className="carousel-item" interval={2000}>
             <img src={ballOnTee} alt="coach instructing baseball drill" />
           </Carousel.Item>
         </Carousel>
+        <img className="logo" src={logoImage} alt="Neighborly Nine Logo" />
       </header>
     );
 }
 
-export default Header; 
+export default Header;
