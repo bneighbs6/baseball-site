a31978439d6082faf3931bc38053eac4ab95f8ee
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-14 16:25:09 -0600

    Edit Home button to go to path "/"

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -7,42 +7,19 @@ function NavigationBar() {
     <Navbar expand="md" variant="light">
       <Container>
         <Nav className="mx-auto">
-          <Nav.Link
-            as={Link}
-            to="header" // Brings user to the top of the webpage if clicked
-            smooth={true}
-            duration={1000}
-          >
+          <Nav.Link href="/">
             <button className="nav-btn">Home</button>
           </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="meetTheCoach"
-            smooth={true}
-            duration={1000}
-          >
+          <Nav.Link as={Link} to="meetTheCoach" smooth={true} duration={1000}>
             <button className="nav-btn">Meet The Coach</button>
           </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="calendly"
-            smooth={true}
-            duration={1000}
-          >
+          <Nav.Link as={Link} to="calendly" smooth={true} duration={1000}>
             <button className="nav-btn">Schedule a Lesson</button>
           </Nav.Link>
-          <Nav.Link
-            as={Link}
-            to="contact"
-            smooth={true}
-            duration={1000}
-          >
+          <Nav.Link as={Link} to="contact" smooth={true} duration={1000}>
             <button className="nav-btn">Contact Us</button>
           </Nav.Link>
-          <Nav.Link
-            href="https://diamond-dev-apparel.printify.me/"
-            target="_blank"
-          >
+          <Nav.Link href="/shop">
             <button className="nav-btn">Shop</button>
           </Nav.Link>
         </Nav>
