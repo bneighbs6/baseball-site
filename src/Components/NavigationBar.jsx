f4c8ba20c5e7e48f61940fe964d35b8356c1fc2d
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-14 14:39:23 -0600

    Edit order of buttons in navigation bar

--- src/Components/NavigationBar.jsx
+++ src/Components/NavigationBar.jsx
@@ -32,19 +32,19 @@ function NavigationBar() {
             <button className="nav-btn">Schedule a Lesson</button>
           </Nav.Link>
           <Nav.Link
-            href="https://diamond-dev-apparel.printify.me/"
-            target="_blank"
-            
-          >
-            <button className="nav-btn">Shop</button>
-          </Nav.Link>
-          <Nav.Link
             as={Link}
             to="contact"
             smooth={true}
             duration={1000}
           >
             <button className="nav-btn">Contact Us</button>
+          </Nav.Link>
+          <Nav.Link
+            href="https://diamond-dev-apparel.printify.me/"
+            target="_blank"
+            
+          >
+            <button className="nav-btn">Shop</button>
           </Nav.Link>
         </Nav>
       </Container>
