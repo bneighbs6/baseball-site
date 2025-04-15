339e2378c8d9f859e97a529f39d4312bfa4b4c45
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-14 20:53:39 -0600

    Made ShopPage Prettier

--- src/Components/Pages/ShopPage.jsx
+++ src/Components/Pages/ShopPage.jsx
@@ -1,19 +1,35 @@
 import React from "react";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHelmetSafety, faPersonDigging, faWrench } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHelmetSafety,
+  faPersonDigging,
+  faWrench,
+} from "@fortawesome/free-solid-svg-icons";
 import Header from "../Header";
 import NavigationBar from "../NavigationBar";
 import Footer from "../Footer";
 
 function ShopPage() {
-    return (
-      <div className="shop-page">
-                <FontAwesomeIcon icon={faPersonDigging} className="construction-icon fa-flip" style={{ '--fa-animation-duration': '3s' }}  />
-        <FontAwesomeIcon icon={faHelmetSafety} className="construction-icon fa-bounce" style={{ '--fa-animation-duration': '3s' }} />
-        <FontAwesomeIcon icon={faWrench} className="construction-icon fa-beat" style={{ "--fa-animation-duration": "3s" }}/>
-        <h5>Shop Page Under Construction</h5>
-      </div>
-    );
+  return (
+    <div className="shop-page">
+      <FontAwesomeIcon
+        icon={faPersonDigging}
+        className="construction-icon fa-flip"
+        style={{ "--fa-animation-duration": "3s" }}
+      />
+      <FontAwesomeIcon
+        icon={faHelmetSafety}
+        className="construction-icon fa-bounce"
+        style={{ "--fa-animation-duration": "3s" }}
+      />
+      <FontAwesomeIcon
+        icon={faWrench}
+        className="construction-icon fa-beat"
+        style={{ "--fa-animation-duration": "3s" }}
+      />
+      <h5>Shop Page Under Construction</h5>
+    </div>
+  );
 }
 
 export default ShopPage;
