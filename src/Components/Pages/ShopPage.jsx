8d11a8ba7c7c628a3625401e4735be35a5e2e7ff
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-14 21:31:07 -0600

    Style ShopPage with Cards for different items

--- src/Components/Pages/ShopPage.jsx
+++ src/Components/Pages/ShopPage.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { Card, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faHelmetSafety,
@@ -12,22 +14,40 @@ import Footer from "../Footer";
 function ShopPage() {
   return (
     <div className="shop-page">
-      <FontAwesomeIcon
-        icon={faPersonDigging}
-        className="construction-icon fa-flip"
-        style={{ "--fa-animation-duration": "3s" }}
-      />
-      <FontAwesomeIcon
-        icon={faHelmetSafety}
-        className="construction-icon fa-bounce"
-        style={{ "--fa-animation-duration": "3s" }}
-      />
-      <FontAwesomeIcon
-        icon={faWrench}
-        className="construction-icon fa-beat"
-        style={{ "--fa-animation-duration": "3s" }}
-      />
-      <h5>Shop Page Under Construction</h5>
+      <div className="shop-apparel-card">
+        <Card className="text-center">
+          <Card.Header>Diamond Dev Apparel</Card.Header>
+          <Card.Body>
+            <Card.Title>Shop Our New Styles Now</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            <Link to="https://diamond-dev-apparel.printify.me/" target="_blank">
+              <Button className="shop-apparel-btn">
+                Diamond Dev Apparel Shop
+              </Button>
+            </Link>
+          </Card.Body>
+          <Card.Footer className="text-muted">Shop Now</Card.Footer>
+        </Card>
+      </div>
+      <div className="shop-strength-training-card">
+        <Card className="text-center">
+          <Card.Header>Strength</Card.Header>
+          <Card.Body>
+            <Card.Title>CSCS Tested & Proven Programs</Card.Title>
+            <Card.Text>
+              With supporting text below as a natural lead-in to additional
+              content.
+            </Card.Text>
+            <Button className="shop-strength-training-btn">
+              Baseball Strength Program
+            </Button>
+          </Card.Body>
+          <Card.Footer className="text-muted">Shop Now</Card.Footer>
+        </Card>
+      </div>
     </div>
   );
 }
