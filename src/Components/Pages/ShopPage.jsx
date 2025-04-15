bb857b9d58190d643b24f7e6990c79ab69546100
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-14 20:52:24 -0600

    Style Shop Page Construction Icons

--- src/Components/Pages/ShopPage.jsx
+++ src/Components/Pages/ShopPage.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHelmetSafety, faPersonDigging } from '@fortawesome/free-solid-svg-icons';
+import { faHelmetSafety, faPersonDigging, faWrench } from '@fortawesome/free-solid-svg-icons';
 import Header from "../Header";
 import NavigationBar from "../NavigationBar";
 import Footer from "../Footer";
@@ -8,9 +8,10 @@ import Footer from "../Footer";
 function ShopPage() {
     return (
       <div className="shop-page">
-        <FontAwesomeIcon icon={faHelmetSafety} className="construction-icon" />
-        <FontAwesomeIcon icon={faPersonDigging} className="construction-icon" />
-        <p>Shop Page Under Construction.</p>
+                <FontAwesomeIcon icon={faPersonDigging} className="construction-icon fa-flip" style={{ '--fa-animation-duration': '3s' }}  />
+        <FontAwesomeIcon icon={faHelmetSafety} className="construction-icon fa-bounce" style={{ '--fa-animation-duration': '3s' }} />
+        <FontAwesomeIcon icon={faWrench} className="construction-icon fa-beat" style={{ "--fa-animation-duration": "3s" }}/>
+        <h5>Shop Page Under Construction</h5>
       </div>
     );
 }
