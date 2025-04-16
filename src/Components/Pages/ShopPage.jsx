ea56b63f58c75f702a44063a8c47e21885d6594b
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-16 07:01:01 -0600

    Rebrand w/ Diamond Dev Logo & alter color scheme

--- src/Components/Pages/ShopPage.jsx
+++ src/Components/Pages/ShopPage.jsx
@@ -17,7 +17,7 @@ function ShopPage() {
       <div className="shop-apparel-card">
         <Card className="text-center">
           <Card.Header>Diamond Dev Apparel</Card.Header>
-          <Card.Img className="shop-card-img" src="../media/Black.png"></Card.Img>
+          <Card.Img className="shop-card-img" src="../media/DiamondDevTransparent.png"></Card.Img>
           <Card.Body>
             <Card.Title>Shop Our New Styles Now</Card.Title>
             <Card.Text>
@@ -41,7 +41,7 @@ function ShopPage() {
       <div className="shop-strength-training-card">
         <Card className="text-center">
           <Card.Header>Strength</Card.Header>
-          <Card.Img className="shop-card-img" src="../media/Transparent.png"></Card.Img>
+          <Card.Img className="shop-card-img" src="../media/DiamondDevTransparent.png"></Card.Img>
           <Card.Body>
             <Card.Title>CSCS Tested & Proven Programs</Card.Title>
             <Card.Text>
