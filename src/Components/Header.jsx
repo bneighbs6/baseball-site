fb9dab4ba1095e40af1181e2f1fabf74fe227483
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-17 15:56:29 -0600

    Remove carousel from header & keep logo only

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CarouselOfImages from "./CarouselOfImages";
+import logoImage from "../media/DiamondDevTransparent2.png"
 
 function Header() { 
     return (
       <header className="header-container">
-        <CarouselOfImages />
+        {/* <CarouselOfImages /> */}
+        <img src={logoImage} className="header-logo" />
       </header>
     );
 }
