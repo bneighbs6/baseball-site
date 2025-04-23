5a42b027509db876c25b9e72fd0bce2c5e3611f1
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-23 14:27:52 -0600

    Edit Footer; Add email envelope anchor tag

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,11 +1,21 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope as faEnvelopeSolid } from "@fortawesome/free-solid-svg-icons";
 
 function Footer() {
     return (
+      <footer>
         <div className="footer">
-            <footer><p>Email: brett.diamonddevelopment@gmail.com</p></footer>
+          <a href="mailto:brett.diamonddevelopment@gmail.com">
+            <FontAwesomeIcon
+              icon={faEnvelopeSolid}
+              className="fa-solid email-icon"
+            />
+          </a>
+          <p>Connect With Us</p>
         </div>
-    )
+      </footer>
+    );
 }
 
-export default Footer; 
+export default Footer;
