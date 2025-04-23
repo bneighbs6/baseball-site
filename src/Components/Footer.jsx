7658334d82362a088e6486771f4ee2c08460ac2e
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-04-23 12:28:21 -0600

    Edit footer to be able to merge

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
     return (
         <div className="footer">
-            <footer><p>Email: brett.diamonddevelopment@gmail.com</p></footer>
+            <footer><p>Email: brett.diamonddevelopment@gmail.com FIX THIS LATER</p></footer>
         </div>
     )
 }
