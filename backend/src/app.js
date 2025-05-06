5a564be3ae253a54eea1e05dc4ac37a1ef69e360
bneighbs6 <152036405+bneighbs6@users.noreply.github.com>
2025-05-06 12:25:46 -0600

    Set up b/e middleware; install Morgan

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,13 @@
 const express = require("express"); // require the Express package and assign it to a variable
 const app = express(); // begin a new Express application and assign it to a variable
 const cors = require("cors"); // require cors and assign it to a variable
+const morgan = require("morgan"); // Require Morgan package for logging useful info to terminal
 
-app.use(cors()); // cors for cross origin reference system security
+const sayHello = (req, res, next) => {
+    res.send("Hello!");
+  };
+
+  app.use(morgan("dev"));
+  app.use(sayHello);
 
 module.exports = app; 
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,7 @@
 const { PORT = 3001 } = process.env; // Set PORT = process.env, or 5000 by default
 const app = require("./app"); // Require the express application from app.js
 
-const listener = console.log(`Listening on Port ${PORT}`); // This function will run when the server successfully starts.
+const listener = () => console.log(`Server is running on Port ${PORT}!`); // This function will run when the server successfully starts.
 
 /* 
 The listen() method in an Express application is what runs the server. 
@@ -9,4 +9,4 @@ It takes two arguments: a port number and a function.
 The PORT variable defines where your server is running, 
 and the listener() function will get called as soon as the server has successfully started
  */
-app.listen(PORT, listener); 
+app.listen(PORT, listener);
